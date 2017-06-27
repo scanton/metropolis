a9a81b6e0f3339f0a0e7efe99806a9d83c00cb9f
scanton <satoricanton@gmail.com>
2017-06-27 15:24:20 -0500

    setting up top nav view

--- views/TopNav.js
+++ views/TopNav.js
@@ -1,48 +1,93 @@
 (function() {
-	var s = '';
-	s += '<nav class="navbar navbar-inverse">';
-	s += '	<div class="container-fluid">';
-	s += '		<div class="navbar-header">';
-	s += '			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".metropolis-navbar-collapse-0" aria-expanded="false">';
-	s += '				<span class="sr-only">Toggle navigation</span>';
-	s += '				<span class="icon-bar"></span>';
-	s += '				<span class="icon-bar"></span>';
-	s += '				<span class="icon-bar"></span>';
-	s += '			</button>';
-	s += '		</div>';
-	s += '		<div class="collapse navbar-collapse metropolis-navbar-collapse-0">';
-	s += '			<ul class="nav navbar-nav">';
-	s += '				<top-nav-dropdown></top-nav-dropdown>';
-	s += '				<li class="active media-option-link option-link" data-target=""><a href="#"><span class="glyphicon glyphicon-cloud-download"></span> Services</a></li>';
-	s += '				<li class="media-option-link option-link" data-target=""><a href="#"><span class="glyphicon glyphicon-ok"></span> Unit Tests</a></li>';
-	s += '				<li class="media-option-link option-link" data-target=""><a href="#"><span class="glyphicon glyphicon-file"></span> Generate API</a></li>';
-	s += '			</ul>';
-	s += '			<ul class="nav navbar-nav navbar-right">';
-	s += '				<li class="refresh-browser-link"><a href="#" v-on:click="refresh"><span class="glyphicon glyphicon-refresh"></span> Refresh</a></li>';
-	s += '				<li class="toggle-dev-tools-link"><a href="#" v-on:click="toggleDevTools"><span class="glyphicon glyphicon-wrench"></span> Dev Tools</a></li>';
-	s += '				<li class="settings-link"><a href="#" v-on:click="toggleSettings"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>';
-	s += '			</ul>';
-	s += '		</div>';
-	s += '	</div>';
-	s += '</nav>';
+  var s = '';
+  s += '<nav class="navbar navbar-inverse">';
+  s += '	<div class="container-fluid">';
+  s += '		<div class="navbar-header">';
+  s += '			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".metropolis-navbar-collapse-0" aria-expanded="false">';
+  s += '				<span class="sr-only">Toggle navigation</span>';
+  s += '				<span class="icon-bar"></span>';
+  s += '				<span class="icon-bar"></span>';
+  s += '				<span class="icon-bar"></span>';
+  s += '			</button>';
+  s += '		</div>';
+  s += '		<div class="collapse navbar-collapse metropolis-navbar-collapse-0">';
+  s += '			<ul class="nav navbar-nav">';
+  s += '				<top-nav-dropdown v-bind:dropdown="dropDown1"></top-nav-dropdown>';
+  s += '				<li class="media-option-link option-link" data-target=""><a href="#"><span class="glyphicon glyphicon-cloud-download"></span> Services</a></li>';
+  s += '				<li class="media-option-link option-link" data-target=""><a href="#"><span class="glyphicon glyphicon-ok"></span> Unit Tests</a></li>';
+  s += '				<li class="media-option-link option-link" data-target=""><a href="#"><span class="glyphicon glyphicon-file"></span> Generate API</a></li>';
+  s += '			</ul>';
+  s += '			<ul class="nav navbar-nav navbar-right">';
+  s += '				<li class="refresh-browser-link"><a href="#" v-on:click="refresh"><span class="glyphicon glyphicon-refresh"></span> Refresh</a></li>';
+  s += '				<li class="toggle-dev-tools-link"><a href="#" v-on:click="toggleDevTools"><span class="glyphicon glyphicon-wrench"></span> Dev Tools</a></li>';
+  s += '				<li class="settings-link"><a href="#" v-on:click="toggleSettings"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>';
+  s += '			</ul>';
+  s += '		</div>';
+  s += '	</div>';
+  s += '</nav>';
 
-	Vue.component('top-nav', {
-		template: s,
-		data: function() {
-			return { 
-				title: 'Metropolis' 
-			}
-		},
-		methods: {
-			refresh: function() {
-				location.reload();
-			},
-			toggleDevTools: function() {
-				remote.getCurrentWindow().toggleDevTools();
-			},
-			toggleSettings: function() {
-				console.log("toggle settings");
-			}
-		}
-	});
+  Vue.component('top-nav', {
+    template: s,
+    data: function() {
+      return {
+        title: 'Metropolis',
+        dropDown1: {
+          title: 'Metropolis',
+          icon: 'glyphicon glyphicon-globe',
+          childLinks: [{
+              title: 'About Metropolis',
+              clickHandler: '{ console.log("clicked"); }'
+            },
+            {
+              type: 'line-break'
+            },
+            {
+              title: 'New Project',
+              clickHandler: function() {
+                console.log("clicked");
+              }
+            },
+            {
+              title: 'Open Project...',
+              clickHandler: function() {
+                console.log("clicked");
+              }
+            },
+            {
+              title: 'Save Project',
+              clickHandler: function() {
+                console.log("clicked");
+              }
+            },
+            {
+              title: 'Save Project As...',
+              clickHandler: function() {
+                console.log("clicked");
+              }
+            },
+            {
+              type: 'line-break'
+            },
+            {
+              title: 'Preferences',
+              clickHandler: function() {
+                console.log("clicked");
+              }
+            }
+          ]
+        }
+      }
+    },
+    methods: {
+      refresh: function() {
+        location.reload();
+      },
+      toggleDevTools: function() {
+        remote.getCurrentWindow().toggleDevTools();
+      },
+      toggleSettings: function() {
+        console.log("toggle settings");
+      }
+    }
+  });
 })();
--- views/TopNavDropdown.js
+++ views/TopNavDropdown.js
@@ -1,18 +1,24 @@
 (function() {
-	var s = '';
-	s += '<li class="dropdown">';
-	s += '	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-globe"></span> Metropolis <span class="caret"></span></a>';
-	s += '	<ul class="dropdown-menu">';
-	s += '		<li><a href="#">New Project</a></li>';
-	s += '		<li><a href="#">Load Project</a></li>';
-	s += '		<li><a href="#">Save Project</a></li>';
-	s += '		<li><a href="#">Save Project As...</a></li>';
-	s += '		<li role="separator" class="divider"></li>';
-	s += '		<li><a href="#">Preferences</a></li>';
-	s += '	</ul>';
-	s += '</li>';
+  var s = '';
+  s += '<li class="dropdown">';
+  s += '	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span v-bind:class="dropdown.icon"></span> {{ dropdown.title }}<span class="caret"></span></a>';
+  s += '	<ul class="dropdown-menu">';
+  s += '		<li v-bind:role="item.type == \'line-break\' ? \'separator\' : \'\'" v-bind:class="item.type == \'line-break\' ? \'divider\' : \'\'"';
+  s += ' v-for="item in dropdown.childLinks" v-bind:onclick="item.clickHandler"><a href="#">{{ item.title }}</a></li>';
 
-	Vue.component('top-nav-dropdown', {
-		template: s
-	});
+//  s += '		<li><a href="#">About Metropolis</a></li>';
+//  s += '		<li role="separator" class="divider"></li>';
+//  s += '		<li><a href="#">New Project</a></li>';
+//  s += '		<li><a href="#">Load Project</a></li>';
+//  s += '		<li><a href="#">Save Project</a></li>';
+//  s += '		<li><a href="#">Save Project As...</a></li>';
+//  s += '		<li role="separator" class="divider"></li>';
+//  s += '		<li><a href="#">Preferences</a></li>';
+  s += '	</ul>';
+  s += '</li>';
+
+  Vue.component('top-nav-dropdown', {
+    template: s,
+    props: ['dropdown']
+  });
 })();
